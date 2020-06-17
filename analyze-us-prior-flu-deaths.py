b38c3c00c1c7c31afb60a55e550dfe8fb83a74e3
lettergram <lettergramm@gmail.com>
2020-06-17 00:24:05 -0500

    making a function

--- analyze-us-prior-flu-deaths.py
+++ analyze-us-prior-flu-deaths.py
@@ -48,23 +48,17 @@ cdc_mortality_file = open(
 
 df = pd.read_csv(cdc_mortality_file)
 
-print(df.columns.values)
-
 select_columns = ['State Abbreviation', 'Year', 'Week',
                   'Week Ending Date', 'Number of Deaths']
 
-# Current week of year 2020
-#current_week = max(df.where(df['Year']==2020)['Week'].dropna())
-
-#df_range = df.where(((df['Week'] >= 1) & (df['Week'] <= current_week)))
 df_range = df.where(df['Type']=='Predicted (weighted)')
 
 df_group_by_date = df_range[select_columns]
 
+# Calculate prior years statistics (2014-2019)
 df_sum_pre_covid_week = df_group_by_date.where(df_group_by_date['Year']!=2020).groupby(
     ['State Abbreviation', 'Year', 'Week']
 ).sum()
-
 df_avg_pre_covid_week = df_sum_pre_covid_week.groupby(['State Abbreviation', 'Week']).mean()
 df_std_pre_covid_week = df_sum_pre_covid_week.groupby(['State Abbreviation', 'Week']).std()
 
@@ -72,19 +66,19 @@ df_std_pre_covid_week = df_sum_pre_covid_week.groupby(['State Abbreviation', 'We
 df_avg_std_pre_covid_week = df_avg_pre_covid_week + df_std_pre_covid_week 
 
 df_covid_week = df_group_by_date.where(df_group_by_date['Year']==2020).groupby(
-    ['State Abbreviation', 'Week']
+    ['State Abbreviation', 'Week', 'Week Ending Date']
 ).sum()
 
-with pd.option_context('display.max_rows', None, 'display.max_columns', 3):
-    print(df_avg_pre_covid_week)
-
-print(len(df_covid_week['Number of Deaths'].values),
-      len(df_avg_pre_covid_week['Number of Deaths'].values))
-
+# Join prior with 2020 stats
 df_covid_week = df_covid_week.merge(df_avg_std_pre_covid_week,
-                                    on=["State Abbreviation", "Week"],
-                                    how='left')
+                                    on=["State Abbreviation", "Week"], how='left')
 
+# Add back end the specific date ending the week
+df_covid_week['Week Ending Date'] = df_group_by_date.where(df_group_by_date['Year']==2020).groupby(
+    ['State Abbreviation', 'Week', 'Week Ending Date']
+).sum().reset_index(level=['Week Ending Date'])['Week Ending Date']
+
+# Rename columns for ease of use / understanding
 df_covid_week = df_covid_week.reset_index(level=['State Abbreviation', 'Week'])
 df_covid_week = df_covid_week.rename(columns={
     "Number of Deaths_x": "Deaths 2020",
@@ -92,22 +86,19 @@ df_covid_week = df_covid_week.rename(columns={
     "State Abbreviation": 'State'
 })
 
-print(df_covid_week.columns)
-
+# Drop year as it's no longer necessary, we know the current year in labels
 df_covid_week = df_covid_week.drop(['Year'], axis=1)
 
+# Find difference between the average + 1 std
 df_covid_week['diff'] = (df_covid_week['Deaths 2020'] - df_covid_week['Deaths (5yr Avg)']).values
-
-
-with pd.option_context('display.max_rows', None, 'display.max_columns', 20):
-    print(df_covid_week.to_string())
-print(df_covid_week.columns)
 
 df_covid_week['normalized_diff'] = np.nan
 
+# Function to noramlize 0 to 1, if less than 0 just set to zero
 def normalize(x, max_val, min_val):
     return max(x / max_val, 0)
 
+# For each state calculate the normal
 for state in df_covid_week['State'].unique():
     state_max = df_covid_week.where(df_covid_week['State']==state)['diff'].max()
     state_min = df_covid_week.where(df_covid_week['State']==state)['diff'].min()
@@ -116,7 +107,31 @@ for state in df_covid_week['State'].unique():
 
     df_covid_week.loc[df_covid_week['State']==state, 'normalized_diff'] = vec
 
-
+state_deaths = {}
+for state in df_covid_week['State'].unique():
+    normalized = df_covid_week.where(df_covid_week['State']==state)['normalized_diff'].dropna().values
+    week_ends = df_covid_week.where(df_covid_week['State']==state)['Week Ending Date'].dropna().values
+    deaths_above_1_std = round(
+        df_covid_week.where(df_covid_week['State']==state)['diff'].sum(),
+        3
+    )
+    deaths =  df_covid_week.where(df_covid_week['State']==state)['Deaths 2020'].sum()    
+    state_deaths[state] = {
+        'deaths': deaths, 
+        'deaths_above_1_std': deaths_above_1_std,
+        'weeks_normalized': normalized.tolist(),
+        'weeks_end': week_ends
+    }
+    
+# Print the table out
 with pd.option_context('display.max_rows', None, 'display.max_columns', 20):
     print(df_covid_week.to_string())
-print(df_covid_week.columns)
+    
+print('US Deaths',
+      round(df_covid_week.where(df_covid_week['State']=='US')['diff'].sum(), 3))
+
+print(state_deaths['IL'])
+
+print(df_covid_week.where(df_covid_week['State']=='US')['normalized_diff'].dropna().values)
+
+print(df_covid_week)
