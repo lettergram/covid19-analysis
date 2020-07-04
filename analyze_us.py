44082e9c5ac8b686253dd3930125f16575124220
lettergram <lettergramm@gmail.com>
2020-07-03 23:02:42 -0500

    updated graphs

--- analyze_us.py
+++ analyze_us.py
@@ -359,6 +359,9 @@ annotated_states = {
         ('05-30', "BLM Protests Start"), 
         ('06-08', "Phase 1"), # https://www.governor.ny.gov/news/governor-cuomo-announces-new-york-city-enter-phase-1-reopening-june-8-and-five-regions-enter
         ('06-24', "Phase 2"), # https://www.washingtontimes.com/news/2020/jun/23/bill-de-blasio-justice-department-spar-over-new-yo/
+        ('07-04', '4th of July'),
+        ('07-05', 'Phase 3'), # https://www.marketwatch.com/story/no-indoor-dining-when-new-york-city-reaches-phase-3-next-monday-2020-07-01
+        
     ],
     "CA": [
         ('04-27', "Phase 0"), # https://covid19.ca.gov/roadmap/
@@ -366,7 +369,9 @@ annotated_states = {
         ('05-25', "Memorial Day"),
         ('05-30', "BLM Protests Start"), 
         ('06-09', "Phase 2"), # https://projects.sfchronicle.com/2020/coronavirus-map/california-reopening/
-        ('06-12', "Phase 3") # https://www.actionnewsnow.com/content/news/Phase-3-of-California-reopening-whats-open-571219961.html
+        ('06-12', "Phase 3"), # https://www.actionnewsnow.com/content/news/Phase-3-of-California-reopening-whats-open-571219961.html
+        ('07-01', "Close Indoor, Again"), # https://covid19.ca.gov/roadmap-counties/
+        ('07-04', '4th of July'),
     ],
     "IL": [
         ('05-05', "Phase 2"), # https://www.ksdk.com/article/news/health/coronavirus/illinois-reopening-plan-regions/63-c8fb6b2e-9de7-4193-a2b3-be3a4a7a3c3b
@@ -374,6 +379,7 @@ annotated_states = {
         ('05-29', "Phase 3"), # https://www.nbcchicago.com/news/local/illinois-phase-3-how-businesses-will-reopen-and-what-you-will-be-allowed-to-do/2278083/
         ('05-30', "BLM Protests Start"),
         ('06-26', "Phase 4"), # https://www.msn.com/en-us/news/us/daywatch-illinois-to-enter-phase-4-on-friday-protests-impact-on-coronavirus-in-chicago-and-laurence-holmes-must-listen-radio/ar-BB15RQs1
+        ('07-04', '4th of July'),
     ],
     "FL": [
         ('05-01', "Phase 1"), # https://www.baynews9.com/fl/tampa/coronavirus/2020/05/01/taking-a-look-at-floridas-phase-2-reopening
@@ -381,6 +387,8 @@ annotated_states = {
         ('05-30', "BLM Protests Start"), 
         ('06-02', "Phase 2"), # https://www.news-press.com/story/news/local/2020/06/04/desantis-phase-2-reopen-florida-what-means/5258774002/
         ('06-05', "Phase 3"), # https://www.msn.com/en-us/video/peopleandplaces/florida-enters-phase-3-on-friday/vi-BB150bdt
+        ('07-03', 'Beach Closures'), # https://www.foxnews.com/transcript/florida-counties-announce-beach-closures-curfews-after-record-high-covid-19-spike
+        ('07-04', '4th of July'),
     ],
     "TX": [
         ('05-01', "Phase 1"), # https://gov.texas.gov/news/post/governor-abbott-announces-phase-one-to-open-texas-establishes-statewide-minimum-standard-health-protocols
@@ -388,6 +396,7 @@ annotated_states = {
         ('05-25', "Memorial Day"),
         ('05-30', "BLM Protests Start"),
         ('06-03', "Phase 3"), # https://www.bizjournals.com/dallas/news/2020/05/29/phase-2-reopening.html
+        ('07-04', '4th of July'),
         
     ],
     "MA": [
@@ -395,23 +404,28 @@ annotated_states = {
         ('05-25', "Memorial Day"),
         ('05-30', "BLM Protests Start"), 
         ('06-08', "Phase 2"), # https://www.masslive.com/coronavirus/2020/06/massachusetts-to-enter-phase-2-of-reopening-plan-on-monday-june-8.html
+        ('07-04', '4th of July'),
+        ('07-05', 'Phase 3'), # https://www.masslive.com/coronavirus/2020/07/massachusetts-to-allow-casinos-gyms-museums-to-reopen-as-early-as-monday-as-state-moves-into-phase-3-of-reopening.html
     ],
     "GA": [
         ('04-24', "Reopens"), # https://www.businessinsider.com/georgia-to-open-gyms-nail-salons-bowling-alleys-and-more-on-friday-2020-4?op=1
         ('05-25', "Memorial Day"),
-        ('05-30', "BLM Protests Start"), 
+        ('05-30', "BLM Protests Start"),
+        ('07-04', '4th of July'),
     ],
     "MI": [
         ('05-07', "Phase 3"), # https://www.mlive.com/public-interest/2020/05/michigan-is-in-phase-3-of-6-in-coronavirus-response-and-recovery-governor-says.html
         ('05-25', "Memorial Day"),
         ('05-30', "BLM Protests Start"),
         ('06-01', "Phase 4"), # https://www.michigan.gov/coronavirus/0,9753,7-406-98163-530627--,00.html apverage - https://www.lansingstatejournal.com/story/news/2020/05/18/reopen-michigan-whitmer-coronavirus-restaurant-bars-retail-phase-safe-start/5215855002/, https://www.freep.com/story/news/local/michigan/2020/05/21/coronavirus-michigan-reopening-whitmer-retail-auto-dental/5235512002/, https://www.clickondetroit.com/news/local/2020/06/01/michigans-reopening-reaches-phase-4-heres-the-next-stage-and-what-it-will-take-to-get-there/
+        ('07-04', '4th of July'),
     ],
     "NJ": [
         ('05-18', "Beaches Open"), # https://www.inquirer.com/things-to-do/jersey-shore/new-jersey-beaches-shore-beach-coronavirus-social-distancing-20200507.html
         ('05-25', "Memorial Day"),
         ('05-30', "BLM Protests Start"),
         ('06-22', "Phase 2"), # https://newyork.cbslocal.com/2020/06/01/new-jersey-to-begin-phase-two-reopening-in-2-weeks-murphy-says/ https://www.inquirer.com/things-to-do/jersey-shore/new-jersey-beaches-shore-beach-coronavirus-social-distancing-20200507.html
+        ('07-04', '4th of July'),
     ]
 }
 
